63689a06801eac72e022c047a8d449ddd2e2bee1
黄豆 <1120089044@qq.com>
2021-08-25 19:48:35 +0800

    完成了个人中心模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,46 @@
-$(function(){
-    // 获取用户基本信息
-    function getUserInfo(){
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            // 请求头配置对象
-            headers: {
-                Authorization: localStorage.getItem('token') || ''
-            },
-            success: res => {
-                if(res.status != 0){
-                    return layui.layer.msg('获取用户信息失败')
-                }
-                renderAvatar(res.data);
-            },
-            // 无论请求是否成功都会调用complete
-            // complete: res => {
-            //     // 在complete中通过res.responseJSON来获取服务器响应的数据
-            //     if(res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！'){
-            //         localStorage.removeItem('token');
-            //         location.href = 'login.html'
-            //     }
-            // }
-        })
+// 获取用户基本信息
+function getUserInfo(){
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // 请求头配置对象
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: res => {
+            if(res.status != 0){
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data);
+        },
+        // 无论请求是否成功都会调用complete
+        // complete: res => {
+        //     // 在complete中通过res.responseJSON来获取服务器响应的数据
+        //     if(res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！'){
+        //         localStorage.removeItem('token');
+        //         location.href = 'login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染头像
+function renderAvatar(user){
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name);
+    if(user.user_pic != null){
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    }else{
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
     }
+}
+
+
+$(function(){  
     getUserInfo();
-
-
-    // 渲染头像
-    function renderAvatar(user){
-        var name = user.nickname || user.username;
-        $('#welcome').html('欢迎&nbsp;&nbsp;'+name);
-        if(user.user_pic != null){
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide();
-        }else{
-            $('.layui-nav-img').hide();
-            var first = name[0].toUpperCase();
-            $('text-avatar').html(first).show();
-        }
-    }
 
     // 退出按钮
     $('#btnLogout').on('click', function(){
